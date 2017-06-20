e35bfc6c7e8ace7b167bbe9dc9a1b93e59b04f4d
Jim Bolla <jim.bolla@intalere.com>
2017-06-20 16:47:57 -0400

    Integrate React app with MVC view.

---
+++ WebApp/ReactApp.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using Manifest = System.Collections.Generic.Dictionary<string, string>;
+
+namespace WebApp
+{
+    public static class ReactApp
+    {
+        // TODO: Read from web.config?
+        private const string BuildFolder = "my-react-app/build";
+
+        // In DEBUG, file is read every request to accomodate fresh JS builds.
+        // In RELEASE, file is read only once for performance.
+#if DEBUG
+        public static Manifest AssetManifest => ReadAssetManifest();
+#else
+        public static readonly Manifest AssetManifest = ReadAssetManifest();
+#endif
+
+        private static Manifest ReadAssetManifest()
+        {
+            var request = HttpContext.Current.Request;
+
+            var manifestPath = Path.Combine(
+                request.PhysicalApplicationPath,
+                BuildFolder,
+                "asset-manifest.json"
+            );
+
+            var assetRoot = Path.Combine(
+                request.ApplicationPath,
+                BuildFolder
+            );
+
+            var json = File.ReadAllText(manifestPath);
+            var manifest = JsonConvert.DeserializeObject<Manifest>(json);
+
+            return manifest.ToDictionary(
+                pair => pair.Key,
+                pair =>
+                {
+                    var assetPath = Path.Combine(assetRoot, pair.Value);
+                    return assetPath.Replace("\\", "/");
+                }
+            );
+        }
+    }
+}
